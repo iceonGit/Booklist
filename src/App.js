79348c4b65448bf07b564f1ad333b6ef8bf1d2ad
iceonGit <icey58506@gmail.com>
2022-10-09 23:12:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BookDetails from "./components/BookDetails";
+import BookForm from "./components/BookForm";
+import BookList from "./components/BookList";
+import Navbar from "./components/Navbar";
+import BookContextProvider from "./contexts/BookContext";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BookContextProvider>
+        <Navbar />
+        <BookList />
+        <BookForm />
+      </BookContextProvider>
     </div>
   );
-}
+};
 
 export default App;
